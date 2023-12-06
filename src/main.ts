81086f9837572cfb37982f76e5df7bbcff6144d5
Thanh Hùng Lý <thanhhung270502@gmail.com>
2023-12-06 21:28:05 +0700

    Test export swagger-file

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  app.useGlobalPipes(new ValidationPipe());
-  process.env.TZ = 'ETC/Universal';
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule, { cors: true });
+    app.useGlobalPipes(new ValidationPipe());
+    process.env.TZ = 'ETC/Universal';
+    const options = new DocumentBuilder().setTitle('Title').setDescription('description').setVersion('1.0').build();
+    const document = SwaggerModule.createDocument(app, options);
+
+    fs.writeFileSync('./swagger-spec.json', JSON.stringify(document));
+    SwaggerModule.setup('/auth', app, document);
+    await app.listen(3000);
 }
 bootstrap();
