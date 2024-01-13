7bcda1cb37fa4fcdc84d0fa7ad33ddff003ff21e
kiethuynhtuan1805 <kiet.huynhtuan1805@gmail.com>
2024-01-13 12:53:29 +0700

    back

--- src/main.ts
+++ src/main.ts
@@ -9,11 +9,12 @@ async function bootstrap() {
     app.useGlobalPipes(new ValidationPipe());
     process.env.TZ = 'ETC/Universal';
     app.enableCors();
-    const swaggerSpecJson = JSON.parse(fs.readFileSync('./swagger-spec.json', 'utf-8'));
+    //const swaggerSpecJson = JSON.parse(fs.readFileSync('./swagger-spec.json', 'utf-8'));
     const options = new DocumentBuilder().setTitle('Title').setDescription('description').setVersion('1.0').build();
-    const document = SwaggerModule.createDocument(app, options, {
-        include: swaggerSpecJson,
-    });
+    //const document = SwaggerModule.createDocument(app, options, {
+    //    include: swaggerSpecJson,
+    //});
+    const document = SwaggerModule.createDocument(app, options);
 
     fs.writeFileSync('/tmp/swagger-spec.json', JSON.stringify(document));
     SwaggerModule.setup('api', app, document);
